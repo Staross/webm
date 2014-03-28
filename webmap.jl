284ca15d00c5fda8513e131b4522838fd3cf40eb
bieler <bieler@upnaemac3.epfl.ch>
2014-03-28 18:27:34 +0100

    getting links works pretty well

--- webmap.jl
+++ webmap.jl
@@ -1,19 +1,23 @@
 using HTTPClient.HTTPC
 using URIParser
 
-
 function getPage(url::String)
 
-    println("Getting url: $url \\n")
+    println("Getting url: $url")
 
     r = HTTPC.get(url,RequestOptions(request_timeout=5.0));
-    if( r.http_code != 200 )
+    if r.http_code != 200
         warn("couldn't read url : $url, HTTP code : $r.http_code")
-        return (false,lowercase(bytestring(r.body)))
+        return (false,"")
+    end
+
+    if r.headers["Content-Type"] != "text/htm"
+        (false,"")
     end
 
     return (true,lowercase(bytestring(r.body)) )
 end
+
 
 function getHost(url::String)
 
@@ -21,53 +25,144 @@ function getHost(url::String)
     return u.host, u.schema
 end
 
-function getLinks(page::String,host::String,schema::String)
+function getLinks(page::String,url::String)
 
-    m = eachmatch(r"href\s*=\s*\"\s*([^\"]+)\s*\""is,page)
+    host,schema = getHost(url)
+
+    #m = eachmatch(r"href\s*=\s*[\"|'|'']\s*([^\"']+)\s*[\"|'|'']"is,page) #don't care about a tag
+
+    m = eachmatch(r"<a[^>]*href\s*=\s*[\"|'|'']\s*([^\"']+)\s*[\"|'|''][^>]*[>|/>]"is,page)
 
     intL = Array(String,0)
     extL = Array(String,0)
-    s = []
-    i=[]
+
     for i in m
         s =  utf8(i.captures[1]);
 
+        if(length(s) < 2)
+            continue
+        end
 
+        #Full links
         if length(s)>3 && s[1:4] == "http"
 
-        u = URI( ascii(s) )
+            u = URI( ascii(s) )
 
             if u.host == host
-                push!(intL, utf8(s) )
+                push!(intL, s )
             else
-                push!(extL, utf8(s) )
+                push!(extL, s )
             end
-
+            continue
         end
 
+        # // links
+        if length(s)>2 && s[1:2] == "//"
+
+            s = string(schema,"://",utf8(s[3:end]))
+            u = URI( ascii(s) )
+
+            if u.host == host
+                push!(intL, s )
+            else
+                push!(extL, s )
+            end
+            continue
+        end
+
+        # / links
         if s[1] == '/'
+
             push!(intL, string(schema,"://",host,"/",utf8(s[2:end])) )
+            continue
         end
-        
 
+        # .. links
+        if length(s)>2 && s[1:3] == "../"
+            #this is where troubles begin
+#            s="../"
+            s = split(s,"../")
+            u = URI( ascii(url) )
 
+            surl = split(u.path,'/')
+            surl = surl[1:end-(length(s))] #remove as many element corresponding to "../"
+
+            #rebuild path
+            link = ""
+            for i=1:length(surl)
+                link = string(link, surl[i],'/')
+            end
+            #finally add the part from the link
+            link = string(link, s[end])
+            link = string(schema,"://",host,link)
+
+            push!(intL, link)
         end
+
+    end
+
+    intL = cleanLinks(intL)
+    extL = cleanLinks(extL)
+
+    intL = unique(intL)
+    extL = unique(extL)
 
     return (intL,extL)
 end
 
-function testGetLinks()
+function cleanLinks(links)
+
+    ext = ["png","jpg","gif","css","js"]
+    isValid = ones(length(links))
+
+    for i = 1:length(links)
+        l = links[i]
+        for ex in ext
+            if length(l) >= length(ex)
+
+                if l[end-length(ex):end] == ".$ex"
+                    isValid[i] = 0
+                end
+            end
+        end
+    end
+
+    links = links[find(isValid)]
+
+    #remove anchors
+    isValid = ones(length(links))
+    for i = 1:length(links)
+        l = links[i]
+        m = match(r"#[^/]*",l)
+
+        if m != nothing
+            l = l[1:m.offset-1]
+            links[i] = l
+        end
+
+    end
+
+    return links
 end
 
 
-url = "http://www.lemonde.fr/"
+url = "http://www.reddit.com/r/adviceanimals/comments/21lrul/i_feel_bad_so_i_have_to_make_up_for_it/"
 
 host,schema = getHost(url)
 sucess,page = getPage(url);
 
-intL,extL = getLinks(page,host,schema)
+intL,extL = getLinks(page,url)
 
-rrefs = [remotecall(1, getPage, intL[i]) for i in 1:max(20,length(intL))]
+#println(intL)
+
+if(true)
+
+for i in 1:min(30,length(intL))
+    rrefs = remotecall(1, getPage, intL[i])
+    sleep(0.01)
+end
+
+end
 
 #ostream=open("tmp.txt", "w+")
 #r=HTTPC.get("http://docs.julialang.org/en/release-0.2/manual/parallel-computing/?highlight=remotecall",RequestOptions(ostream=ostream))
